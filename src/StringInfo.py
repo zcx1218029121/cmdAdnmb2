74f1035f3760c7ca631331de2f84333fb9d3ba35
dh <1218029121@qq.com>
2020-01-01 08:02:17 +0800

    重构

--- src/StringInfo.py
+++
@@ -1,9 +0,0 @@
-from src.BaseItem import *
-
-
-class StringInfo(BaseItem):
-    def __init__(self, item, index=0):
-        super().__init__(item, index=index)
-        self.replys = []
-        for temp in self.item["replys"]:
-            self.replys.append(BaseItem(temp))
